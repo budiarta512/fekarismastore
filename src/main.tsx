824f751a33df3870e0bcb3d552a59e48912c3fed
budiarta512 <budiarta5121@gmail.com>
2023-05-01 13:59:03 +0800

    delete gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter basename="/fekarismastore">
+      <BrowserRouter basename="/">
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
